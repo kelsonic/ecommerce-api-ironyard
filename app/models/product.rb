c22a6c009b09b9736f7e37f5080c85aa47979e28
kelsonic <kgadams93@gmail.com>
2017-08-25 17:38:18 -0400

    Add basic validations for Product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ApplicationRecord
+  validates :title, presence: true, length: { maximum: 500 }
+  validates :description, presence: true, length: { maximum: 500 }
+  validates :price, presence: true, length: { minimum: 2, maximum: 20 }
 end
